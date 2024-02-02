f9526897f0ff17ccad5093b66b886b0f32279991
Dieter Plaetinck <dieter@plaetinck.be>
2024-02-02 17:25:31 +0200

    fix bugs

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -4,10 +4,10 @@ source vars.public
 source vars.secret
 
 mkdir -p deploy-tmp
-rsync -au --delete frontend/ deploy-tmp/
+rsync -a --delete frontend/ deploy-tmp/
 sed -i "s/TURNSTILE-SITEKEY/$sitekey_dev/" deploy-tmp/index.html
 sed -i "s#BASE-URL#$baseurl_dev#" deploy-tmp/index.html
-rsync -au --delete deploy-tmp/ /srv/http/tapas/
+rsync -a --delete deploy-tmp/ /srv/http/tapas/
 
 cd backend
 ./make.sh
